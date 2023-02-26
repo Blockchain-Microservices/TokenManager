2b81e9d978c2f68b667939a8d9d1f7e35ee2afeb
sanixto <sanix.to@gmail.com>
2023-02-26 17:46:22 +0200

    add getAllTokens and getTokenById methods

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { HttpStatus } from '@nestjs/common/enums';
+import { HttpException } from '@nestjs/common/exceptions';
 import { AppService } from './app.service';
+import { Token } from './interfaces/token.interface';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getAll(): Promise<Token[]> {
+    return this.appService.getAllTokens();
+  }
+
+  @Get(':id')
+  async get(@Param('id') id: string): Promise<Token> {
+    const idNum = Number(id);
+
+    if (!idNum) return;
+
+    const token = await this.appService.getTokenById(idNum);
+
+    if (!token)
+      throw new HttpException(
+        `Token with id = ${id} not found`,
+        HttpStatus.NOT_FOUND,
+      );
+
+    return token;
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -4,4 +4,12 @@ import { Token } from './interfaces/token.interface';
 @Injectable()
 export class AppService {
   private readonly tokens: Token[] = [];
+
+  async getAllTokens(): Promise<Token[]> {
+    return this.tokens;
+  }
+
+  async getTokenById(inputId: number): Promise<Token> {
+    return this.tokens.find((token) => token.id === Number(inputId));
+  }
 }
